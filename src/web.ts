5533b406b7205d6b2a457dbe2b1880f3bc637ddc
Muhammad Afif Izzuddin <muhammad.izzuddin@salt.co.id>
2025-05-07 10:53:19 +0700

    feat: add declare global

--- src/web.ts
+++ src/web.ts
@@ -2,9 +2,26 @@ import { WebPlugin } from '@capacitor/core';
 
 import type { CapacitorPluginTestPlugin } from './definitions';
 
+declare global {
+  interface Window {
+    NativeBridge?: {
+      getUserInfo: () => string;
+    };
+  }
+}
+
+
 export class CapacitorPluginTestWeb extends WebPlugin implements CapacitorPluginTestPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async getUserInfo(): Promise<{ name: string; role: string, note: string }> {
+    if (typeof window.NativeBridge?.getUserInfo === 'function') {
+      const json = window.NativeBridge?.getUserInfo();
+      try {
+        return JSON.parse(json);
+      } catch {
+        throw new Error('Invalid JSON returned from NativeBridge');
+      }
+    } else {
+      throw new Error('NativeBridge.getUserInfo is not available');
+    }
   }
 }
